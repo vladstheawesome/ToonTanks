060a574b87730197a5a62f1f85a252110ca7e587
vladstheawesome <vmabhena@gmail.com>
2020-11-03 01:25:05 +0000

    GameMode Blueprint

--- Source/ToonTanks/GameModes/TankGameModeBase.h
+++ Source/ToonTanks/GameModes/TankGameModeBase.h
@@ -28,6 +28,9 @@ public:
 	void ActorDied(AActor* DeadActor);
 
 protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Game Loop")
+	int32 StartDelay = 3;
 	
 	virtual void BeginPlay() override;
 	UFUNCTION(BlueprintImplementableEvent)
